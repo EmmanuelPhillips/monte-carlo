ca298e641643efc4dd3c9642a252fb19e784bcc3
RammusOTP <emmanuelphillips33@gmail.com>
2026-06-29 18:32:33 +0100

    wrote random generator and calculation for whether point is in circle or not

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <random>
 
 int main() {
 
-    return 0;
+  std::random_device rd{};
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> randPos{-1, 1};
+
+  double x{};
+  double y{};
+
+  int insideC{0};
+  int outsideC{0};
+
+  for (int i{0}; i < 1000; ++i) {
+    x = randPos(gen);
+    y = randPos(gen);
+    if ((x * x) + (y * y) <= 1) {
+      ++insideC;
+    } else {
+      ++outsideC;
+    }
+  }
+
+  std::cout << insideC / outsideC;
+
+  return 0;
 }
